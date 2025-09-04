2b7d434bd366239efa5cb1699fc23a8af98d351e
phaedrusflow <map@qompass.ai>
2025-09-03 19:35:05 -0700

    update

--- flake.nix
+++ flake.nix
@@ -1,16 +1,19 @@
 {
-  description = "Hyprland";
+  description = "Qompass AI Hyprland";
   inputs = {
     flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/*";
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-stable.url = "github:nixos/nixpkgs/nixos-25.05";
   };
-  outputs = { self, flake-schemas, nixpkgs }:
+  outputs = { self, flake-schemas, nixpkgs-unstable, nixos-stable, ... }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-darwin" "aarch64-linux" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
         pkgs = import nixpkgs { inherit system; };
       });
-    in {
+    in
+    {
       schemas = flake-schemas.schemas;
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
@@ -20,6 +23,8 @@
             jq
             wget
             nixpkgs-fmt
+            xdg-desktop-portal
+            xdg-desktop-portal-hyprland
           ];
         };
       });
