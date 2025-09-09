c2d4ea1215f257b4167905fca27849ce89e23d5c
phaedrusflow <map@qompass.ai>
2025-09-08 19:43:45 -0700

    update

--- flake.nix
+++ flake.nix
@@ -1,14 +1,19 @@
+# /qompassai/hyprland/flake.nix
+# Qompass AI Hyprland Flake
+# Copyright (C) 2025 Qompass AI, All rights reserved
+####################################################
 {
-  description = "Qompass AI Hyprland";
+  description = "Qompass AI Hyprland Nix Flake";
   inputs = {
     flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/*";
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixos-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+    hyprland.url = "github:hyprwm/Hyprland";
+    nur.url = "github:nix-community/NUR";
+    nur.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, flake-schemas, nixpkgs, nixpkgs-unstable, nixos-stable, ... }:
+  outputs = { self, flake-schemas, nixpkgs, hyprland, nur, ... }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-darwin" "aarch64-linux" ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
         pkgs = import nixpkgs { inherit system; };
       });
@@ -23,10 +28,29 @@
             jq
             wget
             nixpkgs-fmt
-            xdg-desktop-portal
-            xdg-desktop-portal-hyprland
           ];
         };
       });
+      nixosConfigurations = {
+        primo = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            hyprland.nixosModules.default
+            ({ config, pkgs, ... }: {
+              programs.hyprland.enable = true;
+              environment.etc."skel/.config/hypr/hyprland.conf".source = ./hyprland.conf;
+              environment.etc."skel/.config/hypr/hypridle.conf".source = ./hypridle.conf;
+              environment.etc."skel/.config/hypr/hypr.d".source = ./hypr.d;
+              users.users.phaedrus = {
+                isNormalUser = true;
+                extraGroups = [ "wheel" "networkmanager" ];
+                home = "/home/phaedrus";
+              };
+              environment.sessionVariables.NIXOS_OZONE_WL = "1";
+            })
+          ];
+          specialArgs = { inherit hyprland; };
+        };
+      };
     };
 }
