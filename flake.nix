247e2c053ff48991f8431b2a5a4dfdecc8c31c91
phaedrusflow <map@qompass.ai>
2025-09-16 14:00:21 -0700

    update

--- flake.nix
+++ flake.nix
@@ -2,14 +2,17 @@
 # Qompass AI Hyprland Flake
 # Copyright (C) 2025 Qompass AI, All rights reserved
 ####################################################
+# nixos-rebuild switch --flake .#x86_64-linux --argstr username $USER
 {
   description = "Qompass AI Hyprland Nix Flake";
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/*";
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*";
     hyprland.url = "github:hyprwm/Hyprland";
     nur.url = "github:nix-community/NUR";
     nur.inputs.nixpkgs.follows = "nixpkgs";
+    xdg-desktop-portal-hyprland.url = "github:hyprwm/xdg-desktop-portal-hyprland";
+    xdg-desktop-portal-hyprland.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, flake-schemas, nixpkgs, hyprland, nur, ... }:
     let
@@ -25,31 +28,43 @@
           packages = with pkgs; [
             curl
             git
+            hypridle
+            hyprlock
             jq
             wget
             nixpkgs-fmt
+            xdg-desktop-portal-hyprland
           ];
         };
       });
       nixosConfigurations = {
-        primo = nixpkgs.lib.nixosSystem {
+        x86_64-linux = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
             hyprland.nixosModules.default
-            ({ config, pkgs, ... }: {
-              programs.hyprland.enable = true;
-              environment.etc."skel/.config/hypr/hyprland.conf".source = ./hyprland.conf;
-              environment.etc."skel/.config/hypr/hypridle.conf".source = ./hypridle.conf;
-              environment.etc."skel/.config/hypr/hypr.d".source = ./hypr.d;
-              users.users.phaedrus = {
+            ({ config, pkgs, username, ... }: {
+              users.users.${username} = {
                 isNormalUser = true;
                 extraGroups = [ "wheel" "networkmanager" ];
-                home = "/home/phaedrus";
+                home = "/home/${username}";
               };
-              environment.sessionVariables.NIXOS_OZONE_WL = "1";
+              programs.hyprland.enable = true;
+              programs.hyprland.package = pkgs.hyprland;
+              programs.hyprland.portalPackage = pkgs.xdg-desktop-portal-hyprland;
+              programs.hyprlock.enable = true;
+              programs.hyprland.xwayland.enable = true;
+              programs.hyprland.withUWSM = true;
+              environment.sessionVariables = {
+                NIXOS_OZONE_WL = "1";
+                XDG_CONFIG_HOME = "${HOME}/.config";
+                XDG_DATA_HOME = "${HOME}/.local/share";
+                XDG_CACHE_HOME = "${HOME}/.cache";
+                XDG_STATE_HOME = "${HOME}/.local/state";
+                QT_QPA_PLATFORM = "wayland;xcb";
+              };
             })
           ];
-          specialArgs = { inherit hyprland; };
+          specialArgs = { inherit hyprland username; };
         };
       };
     };
