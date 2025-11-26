a1e96c2eb8a56b6308c01883169ce58fdc9fa1e9
phaedrusflow <map@qompass.ai>
2025-11-25 17:18:00 -0800

    Update 2025-11-25

--- scripts/cache_setup.sh
+++
@@ -1,5 +0,0 @@
-# ~/.GH/Qompass/Hyprland/scripts/cache_setup.sh
-# ---------------------------------------------
-# Copyright (C) 2025 Qompass AI, All rights reserved
-
-mkdir -p /$HOME/$USER/.cache/libsycl_cache
